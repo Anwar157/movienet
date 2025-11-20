98074f193f89015a7528c50b428df953300ff6f4
Anwar157 <your-email@example.com>
2025-11-20 15:56:03 +0600

    loging is working

--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router";
+import { AuthContext } from "../../provider/AuthProvider";
+import toast from "react-hot-toast";
 
 const Login = () => {
+  // input field state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // AuthContext to login function taken
+  const { loginUser } = useContext(AuthContext);
+
+  // Navigate hook
+  const navigate = useNavigate();
+
+  // form submit handler
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Firebase Login
+      await loginUser(email, password);
+
+      // Success toast
+      toast.success("Logged in Successfully!");
+
+      // Redirect to home
+      navigate("/");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="hero bg-base-200">
       <div className="hero-content flex flex-col">
@@ -8,12 +39,24 @@ const Login = () => {
           <h1 className="text-2xl md:text-4xl font-bold">Login now!</h1>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form className="card-body" onSubmit={handleLogin}>
             <fieldset className="fieldset">
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                type="email"
+                className="input"
+                placeholder="Email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input
+                type="password"
+                className="input"
+                placeholder="Password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
